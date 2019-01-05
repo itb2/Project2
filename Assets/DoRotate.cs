f4da136fef641aa92caf870f814456a959331db0
Ivory Brown <itb2@duke.edu>
2019-01-05 15:07:25 -0800

    pushing to remote

---
+++ Assets/DoRotate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoRotate : MonoBehaviour
+{
+
+    public float rotationSpeed = 300.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+       
+        if (Input.GetKey("r"))
+        {
+            this.transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            audio.Play();
+        }
+        if(Input.GetKeyUp("r")){
+            audio.Stop();
+        }
+    }
+}
---
+++ Assets/DoRotate2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoRotate2 : MonoBehaviour
+{
+    public float rotationSpeed = 100.0f;
+    double accumulatedTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        accumulatedTime += Time.deltaTime;
+
+        if (accumulatedTime > 15.0)
+            this.transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+       
+    }
+}
